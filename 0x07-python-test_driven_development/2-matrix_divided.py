3bad36b122620f8656f8ec7cdb1dae0876b63237
King CEO <97905873+4mcking@users.noreply.github.com>
2022-09-20 21:44:07 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-This is the "2-matrix_divided" module.
+This is the "2-matrix_divided" module
 
-The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
+The 2-matrix_divided module supplies one function, matrix_divided(matrix, div)
 """
 
 
